98313b3e642d5974488ffd748bd8ae532aff0863
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-05-03 16:01:32 +0100

    That's It

--- paymentmethod/routes.py
+++ paymentmethod/routes.py
@@ -1,33 +1,10 @@
-from flask import current_app as app, jsonify, render_template, request
-import os
-import stripe
-
-stripe_keys = {
-  'secret_key': os.environ['STRIPE_SECRET_KEY'],
-  'publishable_key': os.environ['STRIPE_PUBLISHABLE_KEY']
-}
-
-stripe.api_key = stripe_keys['secret_key']
+from flask import current_app as app, render_template
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html', key=stripe_keys['publishable_key'])
+    return render_template('index.html')
 
-@app.route('/checkout', methods=['POST'])
+@app.route('/checkout')
 def checkout():
-    amount = 50000000
-
-    customer = stripe.Customer.create(
-        email='sample@customer.com',
-        source=request.form['stripeToken']
-    )
-
-    stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency='ngn',
-        description='Flask Charge'
-    )
-
-    return render_template('checkout.html', amount=amount)
+    return render_template('checkout.html')
